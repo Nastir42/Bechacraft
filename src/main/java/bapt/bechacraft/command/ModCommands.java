dcfd282e557eabf09b73f3e37b8df2e4a9066bda
Baptiste Polenor <baptiste.polenor@student-cs.fr>
2023-03-23 08:35:20 +0100

    Changed ModCommands structure

--- src/main/java/bapt/bechacraft/command/ModCommands.java
+++ src/main/java/bapt/bechacraft/command/ModCommands.java
@@ -1,6 +1,7 @@
 package bapt.bechacraft.command;
 
 import com.mojang.brigadier.Command;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 
 import bapt.bechacraft.Bechacraft;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
@@ -12,13 +13,29 @@ public class ModCommands {
     public static final VocationCommand VOCATION = new VocationCommand();
     
     public static <C extends Command<ServerCommandSource>> void registerCommand(C command, String cmd) {
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(CommandManager.literal(cmd)
-        .executes(command)));
+        registerCommand(CommandManager.literal(cmd).executes(command));
+    }
+    
+    public static <C extends Command<ServerCommandSource>> void registerCommand(C command, String ... cmd) {
+        if(cmd.length == 0) {
+            throw new IllegalArgumentException("Arguments expected for command building");
+        }
+        LiteralArgumentBuilder<ServerCommandSource> literal = CommandManager.literal(cmd[cmd.length - 1]).executes(command);
+        for(int i = cmd.length - 2; i >= 0; i--) {
+            literal = CommandManager.literal(cmd[i]).then(literal);
+        }
+        
+        registerCommand(literal);
+    }
+
+    public static <C extends Command<ServerCommandSource>> void registerCommand(LiteralArgumentBuilder<ServerCommandSource> literal) {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal));
     }
 
     public static void registerCommands() {
 
         registerCommand(VOCATION, "vocation");
+        registerCommand(VOCATION, "vocation", "info");
 
         Bechacraft.LOGGER.info("Commands have been initialized");
     }
